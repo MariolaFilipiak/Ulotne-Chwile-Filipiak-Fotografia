70396246764d7b067028c5adf5aa67968fe7a690
Your Name <you@example.com>
2023-02-22 14:56:35 +0100

    content styled

--- src/features/HomePage/Content/styled.js
+++ src/features/HomePage/Content/styled.js
@@ -1,50 +1,93 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-  margin: 20px;
-`;
-export const Title = styled.h2`
-  font-size: 38px;
-  margin-bottom: 50px;
-  letter-spacing: 1.4px;
-`;
-export const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto;
-  align-items: center;
+export const Container = styled.section`
+  background-color: white;
+  text-align: center;
+  padding: 30px;
+  margin: 0 auto;
+  width: 90vw;
 `;
 
+export const Wrapper = styled.div`
+  justify-content: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin: 0 auto;
+  padding: 15px;
+
+  @media (max-width: 1360px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: auto;
+  }
+`;
+
+export const Description = styled.h2`
+  font-size: 40px;
+  color: black;
+  line-height: 1.4;
+  letter-spacing: 1.4px;
+  text-transform: uppercase;
+`;
+
+export const Box = styled.div`
+  -webkit-box-shadow: 0px 0px 5px 1px rgba(240, 240, 240, 1);
+  -moz-box-shadow: 0px 0px 5px 1px rgba(240, 240, 240, 1);
+  box-shadow: 0px 0px 5px 1px rgba(240, 240, 240, 1);
+`;
+
+export const Title = styled.h2`
+  font-size: 25px;
+  letter-spacing: 1.4px;
+
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
+`;
 export const Paragraph = styled.div`
   text-align: center;
   margin-bottom: 50px;
-  padding: 50px;
-  font-size: 20px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+  padding: 10px;
+  font-size: 19px;
   letter-spacing: 1.4px;
   line-height: 1.4;
-  max-width: 80%;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
 `;
+
 export const Images = styled.img`
-  width: 50vw;
-  max-width: 600px;
-  max-height: 600px;
- margin: 20px;
-  padding: 20px;
+  max-width: 100%;
 `;
-
+export const Frame = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    filter: brightness(50%);
+    color: white;
+    transition-duration: 1.5s;
+  }
+`;
 export const Button = styled.button`
-  width: 50vw;
-  width: 120px;
-  height: 50px;
-  background-color: white;
-  border: solid 2px #b9b9b9d9;
-  margin-top: 50px;
+  max-width: 200px;
+  height: 200px;
+  background-color: transparent;
   letter-spacing: 1px;
-  text-align: center;
-
-  &:hover{
-    transform: scale(1.1);
+  position: absolute;
+  border: none;
+  color: transparent;
+  font-size: 25px;
+  line-height: 1.4;
+  letter-spacing: 1.4px;
+  text-transform: uppercase;
+  &:hover {
+    position: absolute;
+    color: white;
+    transition-duration: 2s;
+    cursor: pointer;
   }
 `;
