0deb9bb091d4ba35b500b3ae3fe52c9eecf572c9
Your Name <you@example.com>
2023-02-28 09:00:09 +0100

    change background color

--- src/features/HomePage/Opinions/styled.js
+++ src/features/HomePage/Opinions/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   margin: 0 auto;
   border-top: solid 1px #dfdfdfcf;
-  background-color: #27291b33;
+  background-color: #cdd9ab40;
 `;
 
 export const Paragraph = styled.h2`
