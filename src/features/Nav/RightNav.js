d3e64e96260b4601f4ed801cbcec1b5e712cc7c4
Your Name <you@example.com>
2023-02-28 09:00:21 +0100

    add logo

--- src/features/Nav/RightNav.js
+++ src/features/Nav/RightNav.js
@@ -1,8 +1,12 @@
-import { Element, List, StyledNavLink, Wrapper} from "./styled";
+import { Element, List, StyledLogo, StyledLogo2, StyledNavLink, Wrapper} from "./styled";
+import { ReactComponent as Logo } from "../../common/Images/logo.svg";
 
 export const RightNav = ({ open }) => {
   return (
     <Wrapper>
+      <StyledLogo2>
+        <Logo/>
+      </StyledLogo2>
       <List open={open}>
         <Element>
           <StyledNavLink to="/">Strona Główna</StyledNavLink>
@@ -10,6 +14,9 @@ export const RightNav = ({ open }) => {
         <Element>
           <StyledNavLink to="prices">Oferta</StyledNavLink>
         </Element>
+        <StyledLogo>
+        <Logo/>
+        </StyledLogo>
         <Element>
           <StyledNavLink to="portfolio">Portfolio</StyledNavLink>
         </Element>
