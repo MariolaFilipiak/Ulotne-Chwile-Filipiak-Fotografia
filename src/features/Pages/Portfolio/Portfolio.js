35ae617ee8c1339a3810399ea722afab66eb3763
Your Name <you@example.com>
2023-02-28 08:56:11 +0100

    styled Portfolio add Images, button, title

--- src/features/Pages/Portfolio/Portfolio.js
+++ src/features/Pages/Portfolio/Portfolio.js
@@ -1,10 +1,55 @@
-import { Container, Images, Wrapper } from "./styled";
+import {
+  Wrapper,
+  Container,
+  Content,
+  Header,
+  Text,
+  Button,
+  Images,
+} from "./styled";
 import { Footer } from "../../HomePage/Footer";
+
+import rodzina from "../../../common/Images/photo9.jpg";
+import ciąża from "../../../common/Images/photo21.jpg";
+import pary from "../../../common/Images/photo20.jpg";
+import dzieci from "../../../common/Images/photo13.jpg";
+import reportaże from "../../../common/Images/pary.jpg";
+import indywidualne from "../../../common/Images/photo16.jpg";
 
 export const Portfolio = () => (
   <Container>
+    <Header />
     <Wrapper>
-      <Images />
+      <Content>
+        <Images src={rodzina} />
+        <Text>Sesje rodzinne</Text>
+        <Button>więcej</Button>
+      </Content>
+      <Content>
+        <Images src={dzieci} />
+        <Text>Sesje dziecięce</Text>
+        <Button>więcej</Button>
+      </Content>
+      <Content>
+        <Images src={ciąża} />
+        <Text>sesje ciążowe</Text>
+        <Button>więcej</Button>
+      </Content>
+      <Content>
+        <Images src={pary} />
+        <Text>sesje par</Text>
+        <Button>więcej</Button>
+      </Content>
+      <Content>
+        <Images src={indywidualne} />
+        <Text>sesje indywidualne</Text>
+        <Button>więcej</Button>
+      </Content>
+      <Content>
+        <Images src={reportaże} />
+        <Text>reportaże</Text>
+        <Button>więcej</Button>
+      </Content>
     </Wrapper>
     <Footer />
   </Container>
