d39b25538247bf5eab6512d54c8a9373ab2f94b8
Your Name <you@example.com>
2023-02-28 08:57:42 +0100

    styled AboutMe change width

--- src/features/HomePage/AboutMe/styled.js
+++ src/features/HomePage/AboutMe/styled.js
@@ -3,20 +3,19 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   color: black;
   font-size: 50px;
-  margin: 20px;
   padding: 30px;
   display: grid;
   grid-template-columns: 1fr;
-  justify-content: center;
-  align-content: space-between;
   justify-items: center;
+  width: 100%;
 `;
 export const Photo = styled.img`
-  max-width: 400px;
-  width: 60vw;
+  max-width: 600px;
+  width: 80vw;
   margin: 0 auto;
 `;
 export const Description = styled.p`
+  text-transform: uppercase;
   font-size: 30px;
   text-align: center;
   max-width: 900px;
@@ -25,7 +24,13 @@ export const Description = styled.p`
   line-height: 1.4;
   padding: 20px;
   color: #3c3c3c;
+  @media (max-width: 768px) {
+    font-size: 22px;
+  }
 `;
 export const Paragraph = styled.p`
   font-size: 19px;
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
 `;
