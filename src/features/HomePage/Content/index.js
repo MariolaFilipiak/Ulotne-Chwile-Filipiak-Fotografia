913289a68101c396ba52f0b21d83ade20c346934
Your Name <you@example.com>
2023-02-28 08:58:22 +0100

    styled content remove images

--- src/features/HomePage/Content/index.js
+++ src/features/HomePage/Content/index.js
@@ -1,4 +1,12 @@
-import { Images, Wrapper } from "./styled";
+import {
+  Button,
+  Paragraph,
+  Title,
+  Wrapper,
+  Container,
+  Content,
+  Header,
+} from "./styled";
 
 import photo9 from "../../../common/Images/photo116.jpg";
 import photo6 from "../../../common/Images/photo21.jpg";
@@ -9,15 +17,78 @@ import photo14 from "../../../common/Images/photo123.JPG";
 import photo18 from "../../../common/Images/photo18.jpg";
 import photo8 from "../../../common/Images/photo8.jpg";
 
-export const Content = () => (
-  <Wrapper>
-    <Images src={photo14} alt="sesje par" />
-    <Images src={photo6} alt="sesje ciążowe" />
-    <Images src={photo7} alt="sesje urodzinowe" />
-    <Images src={photo17} alt="sesje noworodkowe" />
-    <Images src={photo9} alt="sesje rodzinne" />
-    <Images src={photo18} alt="reportaże okolicznościowe" />
-    <Images src={photo16} alt="sesje indywidualne" />
-    <Images src={photo8} alt="sesje dziecięce" />
-  </Wrapper>
+export const ContentPage = () => (
+  <Container>
+    <Wrapper>
+      <Content>
+        <Paragraph>
+          <Title>Sesje rodzinne</Title>
+          Sesje rodzinne wykonuję w plenerze i w Waszych domach. Dzieci w każdym
+          wieku mogą brać udział w sesji. A może sesja pokoleniowa? Nie ma
+          problemu!
+        </Paragraph>
+        <Button>Zobacz więcej</Button>
+      </Content>
+      <Content>
+        <Paragraph>
+          <Title>Sesje ciążowe</Title>
+          Sesje ciązowe są niezapomnianą pamiątką zarówno dla przyszłych
+          rodziców jak i dla dziecka.Zapraszam Was - zarówno Mamy jak i Waszych
+          partnerów/ mężów na niezapomniane przeżycie jakim jest sesja ciążowa.
+          A jeśli posiadacie już pociechy to zapraszam razem z nimi.
+        </Paragraph>
+        <Button>Zobacz więcej</Button>
+      </Content>
+      <Content>
+            <Paragraph>
+          <Title>Sesje noworodkowe lifestyle</Title>
+          Sesje noworodkowe najlepiej wykonać do pierwszego miesiąca życia
+          maluszka, jednak sesje lifestalowe dla mnie nie mają takiego
+          ograniczenia. Zdjęcia wykonuję w Waszym domu, gdzie maluch czuje się
+          najlepiej.
+
+        </Paragraph>
+        <Button>Zobacz więcej</Button>
+      </Content>
+      <Content>
+        <Paragraph>
+          <Title>Sesje dziecięce</Title>
+          Sesje dziecięce wykonuję w plenerze. Dzieci tak szybko dorastają,
+          dlatego zdjęcia są jedyną formą która pozwala nam wracać do wspomnień
+          tego pięknego okresu dzieciństwa. Razem stworzymy pamiątkę zarówno dla
+          Was rodziów jak i dla dzieci gdy już dorosną.
+
+        </Paragraph>
+        <Button>Zobacz więcej</Button>
+      </Content>
+
+      <Content>
+        <Paragraph>
+          <Title>Sesje par</Title>
+          Miłość, ah miłość. Uczucia warto uwieczniać na fotografiach. Zapraszam
+          Was w plener lub spotykamy się w Waszych czterech kątach. Podczas
+          sesji uwiecznię wszystko co najpiękniejsze w miłości!
+        </Paragraph>
+        <Button>Zobacz więcej</Button>
+      </Content>
+      <Content>
+        <Paragraph>
+          <Title>Sesje indywidualne</Title>
+          Oczywiście każdy z nas czasem potrzebuje zdjęć z samym sobą. Zdjęcia
+          do portfolio,cv czy na socjalmedia to idelna okazja do wspólnego
+          wyjścia w plener.
+        </Paragraph>
+        <Button>Zobacz więcej</Button>
+      </Content>
+      <Content>
+        <Paragraph>
+          <Title>Reportaże okolicznościowe</Title>
+          Ślub , chrzest dziecka, wieczór panieński a może urodziny? Chętnie
+          uwiecznie te niezapomniane chwile właśnie dla Was. Poniższy cennik nie
+          dotyczy reportaży. Cenę ustalamy indywidualnie
+        </Paragraph>
+        <Button>Zobacz więcej</Button>
+      </Content>
+    </Wrapper>
+  </Container>
 );
--- src/features/HomePage/Content/styled.js
+++ src/features/HomePage/Content/styled.js
@@ -1,63 +1,92 @@
 import styled from "styled-components";
+import background from "../../../common/Images/ozdoba.png";
+import oferta from "../../../common/Images/oferta.png";
+
+
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+`;
 
 export const Wrapper = styled.div`
+  width: 80vw;
   margin: 0 auto;
-  width: 80%;
-  column-count: 3;
- column-gap: 0.6em;
-  margin-bottom: 15px;
+  padding: 20px;
+  display: grid;
+  background-color: white;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  @media (max-width: 1360px) {
+    grid-template-columns: 1fr 1fr ;
+  }
 
   @media (max-width: 990px) {
-    column-count: 3;
-    width: 90%;
-  }
-  @media (max-width: 768px) {
-    column-count: 2;
-    width: 90%;
-  }
-  @media (max-width: 360px) {
-    column-count: 1;
-    width: 90%;
+    grid-template-columns: 1fr;
   }
 `;
 
-export const Images = styled.img`
-  display: inline-block;
-  margin:2px;
-  width: 100%;
-  &:hover {
-    filter: brightness(50%);
-    color: white;
-    transition-duration: 1.5s;
-  }
+export const Content = styled.div`
+  margin: 20px;
+  background-image: url("${background}");
+  background-size: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  border: 1px solid #c8bc9c;
+ height: 450px;
+ display: flex;
+ flex-direction: column;
+ justify-content: space-between;
+ align-items: center;
 `;
-export const Frame = styled.div`
-  //display: flex;
-  // justify-content: center;
-  //align-items: center;
-  &:hover {
-    filter: brightness(50%);
-    color: white;
-    transition-duration: 1.5s;
-  }
-`;
-export const Button = styled.button`
-  max-width: 300px;
-  height: 250px;
-  background-color: transparent;
-  letter-spacing: 1px;
-  position: absolute;
-  border: none;
-  color: transparent;
-  font-size: 25px;
-  line-height: 1.4;
+
+export const Title = styled.h2`
+  font-size: 30px;
   letter-spacing: 1.4px;
-  text-transform: uppercase;
-  text-align: center;
-  &:hover {
-    position: absolute;
-    color: white;
-    transition-duration: 2s;
-    cursor: pointer;
+    line-height: 1.4;
+    color: #3c3c3c;
+
+  @media (max-width: 768px) {
+    font-size: 22px;
   }
+`;
+export const Paragraph = styled.div`
+  text-align: center;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  color: #3c3c3c;
+  padding: 20px;
+  font-size: 18px;
+  letter-spacing: 1.4px;
+  line-height: 1.4;
+
+  @media (max-width: 990px) {
+    width: 100%;
+  }
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  background-color: white;
+  border: 1px solid #c8bc9c;
+  letter-spacing: 1px;
+  color: #3c3c3c;
+  margin-bottom: 20px;
+  &:hover{
+    color:#c8bc9c;
+  }
+`;
+export const Header = styled.div`
+background-image: url("${oferta}");
+  background-attachment: fixed;
+  width: 100%;
+  height: 500px;
+  background-repeat: no-repeat;
+  background-position: center;
 `;
