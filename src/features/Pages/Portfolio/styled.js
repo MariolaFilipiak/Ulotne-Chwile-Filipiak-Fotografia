004075b776b9e7363ae4bf023db2182f776bbab5
Your Name <you@example.com>
2023-02-28 09:01:29 +0100

    add button , images, text

--- src/features/Pages/Portfolio/styled.js
+++ src/features/Pages/Portfolio/styled.js
@@ -1,29 +1,75 @@
 import styled from "styled-components";
+import portfolio from "../../../common/Images/portfolio.png";
+
 
 export const Container = styled.div`
-  width: 100%;
-`;
-export const Images = styled.img`
-  display: inline-block;
-  margin: 0 0 0.3em;
-  width: 100%;
-`;
-export const Wrapper = styled.div`
-  padding: 20px;
   margin: 0 auto;
+  width: 100%;
+  text-align: center;
+`;
+
+export const Wrapper = styled.div`
   width: 80vw;
-  column-count: 3;
-  column-gap: 0.6em;
-  margin-bottom: 15px;
-  padding: 15px;
+  padding: 50px;
+  margin: 0 auto;
+  display: grid;
+  background-color: white;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap:20px;
 
   @media (max-width: 1360px) {
-    column-count: 3;
+    grid-template-columns: 1fr 1fr;
   }
-  @media (max-width: 768px) {
-    column-count: 2;
-  }
-  @media (max-width: 360px) {
-    column-count: 1;
+
+  @media (max-width: 990px) {
+    grid-template-columns: 1fr;
   }
 `;
+export const Header = styled.div`
+  background-image: url("${portfolio}");
+  background-attachment: fixed;
+  width: 100%;
+  height: 600px;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+export const Content = styled.div`
+padding: 20px;
+  border: 1px solid #c8bc9c;
+  display: flex;
+  flex-direction:column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.p`
+  color: #3c3c3c;
+  font-size: 30px;
+  text-transform: uppercase;
+  letter-spacing: 1.4px;
+  line-height: 1.4;
+  text-align: center;
+
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  width: 100px;
+  height: 50px;
+  letter-spacing: 1px;
+  color: #3c3c3c;
+  border: none;
+  text-align: center;
+  border: 1px solid #c8bc9c;
+  &:hover{
+    color:#c8bc9c;
+  }
+`;
+export const Images = styled.img`
+width: 100%;
+border: 1px solid #c8bc9c;
+`;
+export const Box = styled.div`
+`;
+
