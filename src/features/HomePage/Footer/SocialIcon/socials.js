236a774b681e8696a402d17baf3a408a67d3a3ec
Your Name <you@example.com>
2023-03-01 13:46:51 +0100

    socjalIcon edit

--- src/features/HomePage/Footer/SocialIcon/socials.js
+++ src/features/HomePage/Footer/SocialIcon/socials.js
@@ -1,6 +1,5 @@
 import { ReactComponent as instagram } from "./icon/instagram.svg";
 import { ReactComponent as facebook } from "./icon/facebook.svg";
-import { ReactComponent as email } from "./icon/email.svg";
 import { styleIcon } from "./styled";
 
 export const socials = [
@@ -13,10 +12,5 @@ export const socials = [
     name: "facebook",
     url: "https://www.facebook.com/ulotnechwilefilipiakfotografia",
     Icon: styleIcon(facebook),
-  },
-  {
-    name: "email",
-    url: "",
-    Icon: styleIcon(email),
   },
 ];
