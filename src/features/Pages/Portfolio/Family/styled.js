fcdb6b7a2f4c943eaf6551cd37b7b26fd87733c9
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-08-11 08:35:35 +0200

    scale changed

--- src/features/Pages/Portfolio/Family/styled.js
+++ src/features/Pages/Portfolio/Family/styled.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
-export const Container= styled.div`
-margin: 0 auto;
+export const Container = styled.div`
+  margin: 0 auto;
 `;
 export const Wrapper = styled.div`
-column-count: 4 ;
- column-gap:0.6ex;
+  column-count: 4;
+  column-gap: 0.6ex;
   margin-bottom: 15px;
 
   @media (max-width: 990px) {
@@ -20,5 +20,13 @@ column-count: 4 ;
 `;
 
 export const Images = styled.img`
-   width: 100%;
+  width: 100%;
+  transition: opacity 1s linear 0s, transform 1s linear 0s;
+
+  transition: 0.3s;
+  &:hover {
+    opacity: 0.5s;
+    transform: scale(1.1);
+    cursor: pointer;
+  }
 `;
